8c34c883086edffe32ab7127736f0a18ce42bfd6
Raymond Xu <92490442+Bruvato@users.noreply.github.com>
2024-02-13 15:54:52 -0500

    cool

--- scenes/bullet/bullet.gd
+++ scenes/bullet/bullet.gd
@@ -2,8 +2,6 @@ extends Area2D
 
 @export var speed: float = 1000
 
-@export var attack_damage: float = 1.0
-@export var knockback_force: float = 10.0
 
 func _physics_process(delta):
 	var direction: Vector2 = Vector2.RIGHT.rotated(rotation)
@@ -14,9 +12,4 @@ func _on_body_entered(body: Node2D):
 	
 	queue_free()
 	
-	#Signals.emit_signal("bullet_collided", global_position, body.global_position.direction_to(global_position))
-
-func _on_area_entered(area):
-	if area is HurtBoxComponent:
-		var attack = Attack.new(attack_damage, knockback_force, global_position)
-		area.take_damage(attack)
+	Signals.emit_signal("bullet_collided", global_position, body.global_position.direction_to(global_position))
--- scenes/components/attack/attack_component.gd
+++ scenes/components/attack/attack_component.gd
@@ -3,9 +3,11 @@ extends Area2D
 
 @export var attack_damage: float = 1.0
 @export var knockback_force: float = 10.0
+@export var attack_rate: float
+@onready var attack_cooldown: float = 1 / attack_rate
 
 func _on_area_entered(area):
 	if area is HurtBoxComponent:
 		var attack = Attack.new(attack_damage, knockback_force, global_position)
-		#area.take_damage(attack)
-		print_debug("hit")
+		area.take_damage(attack)
+		#print_debug("hit")
